d060e3a885bd283c465bf44415c968beb17af08d
Andy Mueller <andrew.mueller@kroger.com>
2020-12-13 12:34:24 -0500

    some day 13 simplification

--- day_13/day_13b.js
+++ day_13/day_13b.js
@@ -6,39 +6,41 @@ const input = require('fs')
   .map(t => parseInt(t, 10) || t)
 
 const getTimestamp = (busArr, minute, increment) => {
-  let i = minute
   let found = false
 
   while (!found) {
     found = busArr.every(
       (b, idx) =>
-        !Number.isInteger(b) || (Number.isInteger(b) && (i + idx) % b === 0)
+        !Number.isInteger(b) ||
+        (Number.isInteger(b) && (minute + idx) % b === 0)
     )
+
     if (!found) {
-      i += increment
+      minute += increment
     } else {
       return input.length === busArr.length
-        ? i
+        ? minute
         : getTimestamp(
             busArr.concat(
               ...input.slice(
                 busArr.length,
                 input.findIndex(
-                  (b, i) => i >= busArr.length && Number.isInteger(b)
+                  (b, idx) => idx >= busArr.length && Number.isInteger(b)
                 ) + 1
               )
             ),
-            i,
+            minute,
             busArr.reduce((acc, b) => acc * (Number.isInteger(b) ? b : 1))
           )
     }
   }
 }
 
-const answer = getTimestamp(
-  input.slice(0, input.findIndex((b, i) => i > 0 && Number.isInteger(b)) + 1),
-  0,
-  1
+console.log(
+  'success!',
+  getTimestamp(
+    input.slice(0, input.findIndex((b, i) => i > 0 && Number.isInteger(b)) + 1),
+    0,
+    1
+  )
 )
-
-console.log('success!', answer)
